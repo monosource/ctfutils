78ce913b8828c4644ca4eddec263caab9b78a7cc
monosource <monosource@users.noreply.github.com>
2016-09-14 19:04:48 +0300

    Added substitution helper

--- ctfutils/stringutils.py
+++ ctfutils/stringutils.py
@@ -5,7 +5,7 @@ def chunks(l,n):
         yield l[i:i+n]
 
 def strxor(s1, s2, out='string'):
-    """XOR two strings together
+    """XOR two strings together.
     """
     xored = [chr(ord(x1) ^ ord(x2)) for (x1, x2) in zip(s1, s2)]
     if out == 'string':
@@ -24,10 +24,49 @@ def bindecode(s, out='string'):
         return dec
 
 def binencode(s, out='string'):
-    """Encode as string of bits
+    """Encode as string of bits.
     """
     enc = map(lambda x: bin(ord(x))[2:].rjust(8, '0'), s)
     if out == 'string':
         return ''.join(enc)
     elif out == 'list':
         return enc
+
+def replace(s, old, new):
+    """Generalized substitution.
+    old an new are lists of substrings to be replaced
+    """
+    res = s
+    for k, v in zip(old, new):
+        res = res.replace(k, v)
+    return res
+
+def subst(inp_string, alpha, subst):
+    """Substitute alphabet in inp_string (alpha) with subst alphabet
+    """
+    d={}
+    for idx_c, c in enumerate(alpha):
+        d[c] = subst[idx_c]
+    return ''.join(d[c] if c in d else c for c in inp_string)
+
+
+'''
+def morse(s, dash='-'):
+    """Encode string as Morse code string.
+    """
+    alph = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
+    mcodes = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--',
+                '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..',
+                '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.', '-----']
+    return replace(s, alph, mcodes)
+
+
+def morsedec(s):
+    """Decode morse string.
+    """
+    alph = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
+    mcodes = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--',
+                '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..',
+                '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.', '-----']
+    return replace(s, mcodes, alph)
+'''
